c2e87e141010826d91f7bab948eb23e5cd9188ea
algmoreno <alg.moreno00@gmail.com>
2021-05-21 17:23:53 -0700

    if statements consolidated and generator woring

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,48 @@
 // Assignment code here
-
+var charset = ""; 
+var retVal = "";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate") 
 
 // var gatherInfo = function() {
+
+
+
+var generatePassword = function() {
   var inputLength = window.prompt("Choose a number between 8-128 to be your password length"); 
   
-    if (inputLength > 8 || inputLength < 128 ) {
-    } 
+  if (inputLength > 8 || inputLength < 128 ) {
+  } 
 
-    // if (inputLength < 8 || inputLength > 128 || inputLength === "") {
-    //   var inputLength = window.prompt("Please enter a valid response.") 
-    //   return false; 
-    // }
+  if (inputLength < 8 || inputLength > 128 || inputLength === "") {
+    alert("Please enter a valid response.") 
+    return null; 
+  }
 
-  
+
 
 var confirmLower = confirm("Press 'Ok' if you want lower case letters in your password");
 var confirmUpper = confirm("Press 'Ok' if you want upper case letters in your password");
 var confirmNumber = confirm("Press 'Ok' if you want numbers in your password");
 var confirmSpecial = confirm("Press 'Ok' if you want special characters in your password");
 
-
-var generatePassword = function() {
   var length = inputLength; 
 
   if (confirmLower) {
-    charset = "abcdefghijklmnopqrstuvwxyz",
-    retVal = ""; 
-}
-  else if (confirmUpper) {
-    charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    retVal = ""; 
-}
-  else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!\"#$%&',()*+-./:;<>=@[]`{}|~",
-    retVal = "";  
-}
-  else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!\"#$%&',()*+-./:;<>=@[]`{}|~",
-    retVal = ""; 
-}
-  else if (confirmLower && confirmUpper && confirmNumber) {
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789",
-    retVal = ""; 
-}
-  else if (confirmLower && confirmUpper) {
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    retVal = ""; 
-}
-  else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!\"#$%&',()*+-./:;<>=@[]`{}|~",
-    retVal = ""; 
-}
-  
+    charset += "abcdefghijklmnopqrstuvwxyz"
+  }
+
+  if (confirmUpper) {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+
+  if (confirmNumber) {
+    charset += "123456789"
+  }
+
+  if (confirmSpecial) {
+    charset += "!\"#$%&',()*+-./:;<>=@[]`{}|~"
+  }
   
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
@@ -64,6 +53,9 @@ var generatePassword = function() {
 // Write password to the #password input
 var writePassword = function() {
   var password = generatePassword();
+  if (password === null) {
+      password = generatePassword(); 
+  }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -71,5 +63,3 @@ var writePassword = function() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
-
-writePassword(); 
